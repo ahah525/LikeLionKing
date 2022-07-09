29d0ed9fc0688fa86dd81d10087b4e71ad671d27
ahah525 <ahah525@naver.com>
2022-07-09 23:38:08 +0900

    Post domain 구현

--- src/main/java/com/hsy/likelion/LikeLionKing/domain/Post.java
+++ src/main/java/com/hsy/likelion/LikeLionKing/domain/Post.java
@@ -1,31 +1,12 @@
 package com.hsy.likelion.LikeLionKing.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Post {
-    private Long id;
-    private String title;
-    private String content;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private Long id;        // 게시글 id
+    private String title;   // 게시글 제목
+    private String content; // 게시글 내용
 }
