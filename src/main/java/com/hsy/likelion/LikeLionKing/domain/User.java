9f12610dcc2810269b3c70ddfd61eac81637081e
ahah525 <ahah525@naver.com>
2022-07-10 19:30:55 +0900

    User domain 구현

---
+++ src/main/java/com/hsy/likelion/LikeLionKing/domain/User.java
@@ -0,0 +1,12 @@
+package com.hsy.likelion.LikeLionKing.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private Long id;        // 회원 id
+    private String loginId; // 회원 로그인 id
+    private String loginPw; // 회원 로그인 pw
+}
