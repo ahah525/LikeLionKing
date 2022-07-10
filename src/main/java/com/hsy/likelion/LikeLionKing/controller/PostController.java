4ee4f4919f660a4da783a52f4ecc5ee91922e882
ahah525 <ahah525@naver.com>
2022-07-10 23:44:25 +0900

    MemberRepository 구현

--- src/main/java/com/hsy/likelion/LikeLionKing/controller/PostController.java
+++ src/main/java/com/hsy/likelion/LikeLionKing/controller/PostController.java
@@ -1,8 +1,8 @@
 package com.hsy.likelion.LikeLionKing.controller;
 
 import com.hsy.likelion.LikeLionKing.domain.Post;
-import com.hsy.likelion.LikeLionKing.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
+import com.hsy.likelion.LikeLionKing.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,41 +14,46 @@ import java.util.Optional;
 // @RestController = @Controller + @ResponseBody
 // Controller로 데이터 반환하기(JSON 형태의 객체 데이터반환에 주로 사용)
 @RestController
-@RequiredArgsConstructor    // 생성자 주입
+@RequestMapping("/usr/article")
 public class PostController {
-    private final PostRepository postRepository;
+    private final PostService postService;
 
-    @PostMapping("/usr/article/doWrite")
-    public ResponseEntity<Post> createPost(@RequestParam("title")String title, @RequestParam("content")String content) {
-        Post post = new Post();
-        post.setTitle(title);
-        post.setContent(content);
-        return ResponseEntity.status(HttpStatus.OK).body(postRepository.save(post));
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
     }
 
-    @GetMapping("/usr/article/detail")
+    @PostMapping("/doWrite")
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+//        Post post = new Post();
+//        post.setTitle(title);
+//        post.setContent(content);
+        return ResponseEntity.status(HttpStatus.OK).body(postService.save(post));
+    }
+
+    @GetMapping("/detail")
     public ResponseEntity<Optional<Post>> getDetailPost(@RequestParam("id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(postRepository.findById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(postService.findById(id));
     }
 
-    @GetMapping("usr/article")
+    @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
-        return ResponseEntity.status(HttpStatus.OK).body(postRepository.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(postService.findAll());
     }
 
-    @GetMapping("usr/article/doModify")
-    public String modifyPost(@RequestParam("id")Long id, @RequestParam("title")String title, @RequestParam("content")String content) {
-        Post post = new Post();
-        post.setId(id);
-        post.setTitle(title);
-        post.setContent(content);
-        postRepository.update(post);
-        return title;
+    @PutMapping("/doModify")
+    public Post modifyPost(@RequestBody Post post) {
+//        Post post = new Post();
+//        post.setId(id);
+//        post.setTitle(title);
+//        post.setContent(content);
+        postService.update(post);
+        return post;
     }
 
-    @DeleteMapping("usr/article/{postId}")
+    @DeleteMapping("/{postId}")
     public Long deletePost(@PathVariable("postId") Long postId) {
-        postRepository.delete(postId);
+        postService.delete(postId);
         return postId;
     }
 }
